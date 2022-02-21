b93348e0e9bd7c3be107ea9b6e062d15b405019d
Nikola Knežević <48497634+knez@users.noreply.github.com>
2022-02-21 14:53:25 +0100

    More granular test suite

--- test/test_maldump.py
+++ test/test_maldump.py
@@ -1,29 +1,49 @@
 import os
 import unittest
 from datetime import datetime
-from hashlib import md5
 
 from maldump.av_manager import AVManager
 
-class TestOutput(unittest.TestCase):
+
+class TestExport(unittest.TestCase):
 
     # Prepare environment
-    def setUpClass():
+    @classmethod
+    def setUpClass(cls):
         os.chdir('test/root')
+        cls.avs = [av.export() for av in AVManager.avs]
 
-    def test_export(self):
-        for av in AVManager.avs:
-            for entry in av.export():
-                #self.assertIsInstance(entry.timestamp, datetime)
+    def test_list_not_empty(self):
+        self.assertIsNotNone(self.avs)
+
+    def test_timestamp(self):
+        for av in self.avs:
+            for entry in av:
+                self.assertIsInstance(entry.timestamp, datetime)
+
+    def test_path_contains_eicar(self):
+        for av in self.avs:
+            for entry in av:
                 self.assertIsNotNone(entry.path)
                 self.assertIn('eicar', entry.path)
+
+    def test_file_size(self):
+        for av in self.avs:
+            for entry in av:
                 self.assertEqual(68, entry.size)
+
+    def test_md5_hash(self):
+        for av in self.avs:
+            for entry in av:
                 self.assertEqual(entry.md5, '44d88612fea8a8f36de82e1278abb02f')
+
+    def test_file_is_eicar(self):
+        for av in self.avs:
+            for entry in av:
                 self.assertIsInstance(entry.malfile, bytes)
-                self.assertEqual
-                (
+                self.assertEqual(
                     entry.malfile,
-                    b'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+                    br'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
                 )
 
 
