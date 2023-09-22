b15cdeef2383759eb1ea35ab170a6a75c7dd120d
Jonáš Novotný <novj@int.govcert.cz>
2023-09-22 10:29:56 +0200

    Change version to v0.3.0

--- maldump/__main__.py
+++ maldump/__main__.py
@@ -17,7 +17,7 @@ from colorama import Fore, Style, init
 from maldump.av_manager import AVManager
 from maldump.types import Quarantine
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 
 def main() -> None:
