8a171dc03762d7c46a2418a402c68d6b88219875
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-03-14 10:36:04 +0100

    Add time converter class for win and unix times

--- maldump/utils.py
+++ maldump/utils.py
@@ -1,6 +1,7 @@
 """
 Convenience utils for use in avs and parsers
 """
+from datetime import datetime, UTC
 
 from arc4 import ARC4
 
@@ -20,3 +21,25 @@ class CustomArc4:
     def decode(self, plaintext: bytes) -> bytes:
         cipher = ARC4(self.key)
         return cipher.decrypt(plaintext)
+
+class TimeConverter:
+    def __init__(self, time_type: str):
+        self.time_type = time_type
+
+    def _decode_windows(self, wintime_bytes):
+        wintime = int.from_bytes(wintime_bytes, byteorder="little")
+        magic_number = 11644473600
+        timestamp = (wintime // 10000000) - magic_number
+        return datetime.fromtimestamp(timestamp, tz=UTC)
+
+    def _decode_unix(self, unixtime_bytes):
+        timestamp = int.from_bytes(unixtime_bytes, byteorder="little")
+        return datetime.fromtimestamp(timestamp, tz=UTC)
+
+    def decode(self, wintime_bytes: bytes) -> datetime:
+        if self.time_type == "windows":
+            return self._decode_windows(wintime_bytes)
+        elif self.time_type == "unix":
+            return self._decode_unix(wintime_bytes)
+        else:
+            raise NotImplementedError
