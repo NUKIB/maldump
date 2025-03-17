f844ae000b94b1edfd08be2d87f4c75037fa6bd2
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-03-17 11:39:02 +0100

    Rewrite the solution to work with the new Template pattern in Avira

--- maldump/parsers/avira_parser.py
+++ maldump/parsers/avira_parser.py
@@ -11,11 +11,9 @@ if TYPE_CHECKING:
 
 
 class AviraParser(Parser):
-    def from_file(self, name: str, location: Path) -> list[QuarEntry]:
-        self.name = name
-        self.location = location
 
-        quarfiles = []
+    def parse_from_log(self, name: str, location: Path, data: dict[str, QuarEntry] = None) -> dict[str, QuarEntry]:
+        quarfiles = {}
         for metafile in self.location.glob("*.qua"):
             kt = KaitaiParser.from_file(metafile)
             q = QuarEntry()
@@ -23,7 +21,10 @@ class AviraParser(Parser):
             q.threat = kt.mal_type
             q.path = kt.filename[4:]
             q.malfile = kt.mal_file
-            quarfiles.append(q)
+            quarfiles[str(metafile)] = q
             kt.close()
 
         return quarfiles
+
+    def parse_from_fs(self, name: str, location: Path, data: dict[str, QuarEntry] = None) -> dict[str, QuarEntry]:
+        pass
