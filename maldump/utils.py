cfba55de76d868ebcdfbfee82d52d5f2a7053a73
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-04-14 00:04:39 +0200

    Add Reader and Parser to utils.py
    This step is done to make opening the files and parsing the kaitai structs and datetime objects easier to maintain and easier to log.

--- maldump/utils.py
+++ maldump/utils.py
@@ -5,10 +5,15 @@ Convenience utils for use in avs and parsers
 import contextlib
 import logging
 from datetime import datetime, timezone
+from pathlib import Path
+from typing import Type, Generic, TypeVar
 
+import kaitaistruct
 from arc4 import ARC4
 
 from maldump.constants import OperatingSystem
+
+T = TypeVar("T")
 
 
 def xor(plaintext: bytes, key: bytes) -> bytes:
@@ -64,3 +69,68 @@ class DatetimeConverter:
             logging.debug("Birthtime extracted successfully")
 
         return datetime.fromtimestamp(ctime // 1000000000)
+
+
+class Parser(Generic[T]):
+    def __init__(self, obj: object) -> None:
+        self.objname = obj.__class__.__name__
+
+    def kaitai(self, kaitai: Type[T], path: Path) -> T | None:
+        kt = None
+        try:
+            logging.debug(
+                'Trying to parse file on path "%s" to kaitai struct of type "%s" on <%s>',
+                path,
+                kaitai.__name__,
+                self.objname,
+            )
+            kt = kaitai.from_file(path)
+        except OSError as e:
+            logging.exception(
+                'Cannot open nor read kaitai for path "%s"',
+                path,
+                exc_info=e,
+            )
+        except kaitaistruct.KaitaiStructError as e:
+            logging.warning(
+                'Cannot read kaitai, probably incorrect format for path "%s"',
+                path,
+                exc_info=e,
+            )
+        return kt
+
+    def timestamp(self, value: int) -> datetime:
+        try:
+            logging.debug(
+                "Trying to convert timestamp on <%s>",
+                self.objname,
+            )
+            timestamp = datetime.fromtimestamp(int(value))
+        except (OSError, OverflowError, ValueError) as e:
+            logging.warning(
+                "Cannot convert timestamp to datetime, using default",
+                exc_info=e,
+            )
+            timestamp = datetime.now()
+
+        return timestamp
+
+
+class Reader:
+    @staticmethod
+    def contents(path: Path, filetype: str = ""):
+        if filetype:
+            filetype += " "
+
+        try:
+            logging.debug('Trying to open %sfile, path "%s"', filetype, path)
+            with open(path, "rb") as f:
+                logging.debug('Trying to read %sfile, path "%s"', filetype, path)
+                data = f.read()
+        except OSError as e:
+            logging.exception(
+                'Cannot open %sfile in ESET on path "%s"', filetype, path, exc_info=e
+            )
+            data = None
+
+        return data
