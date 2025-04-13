cb6ba8c6f63bb26ffaa4de5a36b3f755eaeaa8c8
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2025-04-14 00:14:55 +0200

    Add new logging techniques to Avira

--- maldump/parsers/avira_parser.py
+++ maldump/parsers/avira_parser.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
 
-from datetime import datetime as dt
+import logging
 
 from maldump.parsers.kaitai.avira_parser import AviraParser as KaitaiParser
 from maldump.structures import Parser, QuarEntry
+from maldump.utils import Parser as parse
 
 
 class AviraParser(Parser):
     def parse_from_log(self, _=None) -> dict[str, QuarEntry]:
+        logging.info("Parsing from log in %s", self.name)
         quarfiles = {}
-        for metafile in self.location.glob("*.qua"):
-            kt = KaitaiParser.from_file(metafile)
+        for idx, metafile in enumerate(self.location.glob("*.qua")):
+            logging.debug('Parsing entry, idx %s, path "%s"', idx, metafile)
+
+            kt = parse(self).kaitai(KaitaiParser, metafile)
+            if kt is None:
+                logging.debug('Skipping entry idx %s, path "%s"', idx, metafile)
+                continue
+
             q = QuarEntry()
-            q.timestamp = dt.fromtimestamp(kt.qua_time)
+
+            q.timestamp = parse(self).timestamp(kt.qua_time)
             q.threat = kt.mal_type
             q.path = kt.filename[4:]
             q.malfile = kt.mal_file
